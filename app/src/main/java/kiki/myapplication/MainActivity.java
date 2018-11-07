8f49a3d237f2faec436b16f5d3ef97ee530eee60
Kirsten <kapeine@gmx.de>
2018-11-07 13:53:56 +0100

    Initial commit

---
+++ app/src/main/java/kiki/myapplication/MainActivity.java
@@ -0,0 +1,31 @@
+package kiki.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import kiki.myapplication.ActivityDataSource;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
+        Button lesenBtn = (Button) findViewById(R.id.lesenBtn);
+        lesenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ActivityDataSource(textView).execute("someParams");
+            }
+        });
+    }
+
+}
