d7267a1ecbfa972c44dc5dd4faf0b7abd9497982
serdar <serdarsarsar@gmx.de>
2018-11-08 19:57:18 +0100

    der verbindungsaufbaudings muss in einer Klasse gemacht werden, welche AsyncTask erweitert. Wegen solchen Sachen klappt das am Ende wieder nicht also achte mehr auf sowas

--- app/src/main/java/kiki/myapplication/TestActivity.java
+++ app/src/main/java/kiki/myapplication/TestActivity.java
@@ -1,5 +1,6 @@
 package kiki.myapplication;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -38,68 +39,76 @@ public class TestActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String result = null;
-                InputStream is = null;
-                StringBuilder sb=null;
 
-                //Verbindung zum Server
-                try{
-
-                    URL url = new URL("http://localhost:8080/quercus-4.0.39/verbindung.php");
-                    URLConnection con = url.openConnection();
-
-                    //TODO: Alles was hier drunter grau ist, koennen wir nicht nutzen, da es veraltet ist.
-                    //TODO: Die ALternative waere wohl das, welches ich oben bereits begonne habe (Url url... URLCOnnection)
-                    //TODO: Also muessen wir uns das zusammen basteln das es die selbe Aussage spaeter hat
-
-                    //HttpClient httpclient = new DefaultHttpClient();
-                    //HttpPost httppost = new HttpPost("http://domain.com/AndroidTest/index.php");
-                    //HttpResponse response = httpclient.execute(httppost);
-                    //HttpEntity entity = response.getEntity();
-                    //is = entity.getContent();
-                }catch(Exception e){
-                    Log.e("log_tag", "Verbindungsfehler"+e.toString());
-                }
-
-                //konvertieren der Antwort in einen String
-                try{
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(is,"utf-8"),8);
-                    sb = new StringBuilder();
-                    sb.append(reader.readLine() + "\n");
-                    String line="0";
-                    while ((line = reader.readLine()) != null) {
-                        sb.append(line + "\n");
-                    }
-                    is.close();
-                    result=sb.toString();
-                }catch(Exception e){
-                    Log.e("log_tag", "Error converting result "+e.toString());
-                }
-
-                //Daten trennen
-                int fd_id = 0;
-                String fd_benutzername = "";
-                String fd_name = "";
-                String fd_passwort = "";
-
-                try{
-                    JSONArray jArray = new JSONArray(result);
-                    JSONObject json_data=null;
-                    for(int i=0;i<jArray.length();i++) {
-                        json_data = jArray.getJSONObject(i);
-                        fd_id = json_data.getInt("BenutzerID");
-                        fd_benutzername = json_data.getString("Bentzername");
-                        fd_name = json_data.getString("Name");
-                        fd_passwort = json_data.getString("Passwort");
-                    }
-                }catch(JSONException e1){
-                    Toast.makeText(getBaseContext(), "Keine Daten oder keine Verbindung!", Toast.LENGTH_LONG).show();
-                }
-
-                //Ausgeben der empfangenen Daten
-                text.setText("ID: "+fd_id+" Benutzername: "+fd_benutzername+" Name: "+fd_name+" Passwort: "+fd_passwort);
             }
         });
 
+    }
+    class Verbindung extends AsyncTask {
+
+        @Override
+        protected Object doInBackground(Object[] objects) {
+            String result = null;
+            InputStream is = null;
+            StringBuilder sb=null;
+
+            //Verbindung zum Server
+            try{
+
+                URL url = new URL("http://localhost:8080/quercus-4.0.39/verbindung.php");
+                URLConnection con = url.openConnection();
+
+                //TODO: Alles was hier drunter grau ist, koennen wir nicht nutzen, da es veraltet ist.
+                //TODO: Die ALternative waere wohl das, welches ich oben bereits begonne habe (Url url... URLCOnnection)
+                //TODO: Also muessen wir uns das zusammen basteln das es die selbe Aussage spaeter hat
+
+                //HttpClient httpclient = new DefaultHttpClient();
+                //HttpPost httppost = new HttpPost("http://domain.com/AndroidTest/index.php");
+                //HttpResponse response = httpclient.execute(httppost);
+                //HttpEntity entity = response.getEntity();
+                //is = entity.getContent();
+            }catch(Exception e){
+                Log.e("log_tag", "Verbindungsfehler"+e.toString());
+            }
+
+            //konvertieren der Antwort in einen String
+            try{
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is,"utf-8"),8);
+                sb = new StringBuilder();
+                sb.append(reader.readLine() + "\n");
+                String line="0";
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                is.close();
+                result=sb.toString();
+            }catch(Exception e){
+                Log.e("log_tag", "Error converting result "+e.toString());
+            }
+
+            //Daten trennen
+            int fd_id = 0;
+            String fd_benutzername = "";
+            String fd_name = "";
+            String fd_passwort = "";
+
+            try{
+                JSONArray jArray = new JSONArray(result);
+                JSONObject json_data=null;
+                for(int i=0;i<jArray.length();i++) {
+                    json_data = jArray.getJSONObject(i);
+                    fd_id = json_data.getInt("BenutzerID");
+                    fd_benutzername = json_data.getString("Bentzername");
+                    fd_name = json_data.getString("Name");
+                    fd_passwort = json_data.getString("Passwort");
+                }
+            }catch(JSONException e1){
+                Toast.makeText(getBaseContext(), "Keine Daten oder keine Verbindung!", Toast.LENGTH_LONG).show();
+            }
+
+            //Ausgeben der empfangenen Daten
+            text.setText("ID: "+fd_id+" Benutzername: "+fd_benutzername+" Name: "+fd_name+" Passwort: "+fd_passwort);
+            return null;
+        }
     }
 }
