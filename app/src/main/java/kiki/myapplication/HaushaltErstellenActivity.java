9f939638173cd4168732d27dbe0e4e5a98cea7f7
Kirsten <kapeine@gmx.de>
2018-11-26 18:58:16 +0100

    Haushalterstellen Activity bearbetet (Layout und in der java Klasse Ressourcen hinzugefügt)

--- app/src/main/java/kiki/myapplication/HaushaltErstellenActivity.java
+++ app/src/main/java/kiki/myapplication/HaushaltErstellenActivity.java
@@ -2,15 +2,39 @@ package kiki.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.Objects;
 
 public class HaushaltErstellenActivity extends AppCompatActivity {
+
+    EditText et_haushalterstellenname, et_haushalterstellenanschrift, et_haushalterstellenbeschreibung, et_haushalterstellenpasswort;
+    Button btn_haushalterstellen;
+
+    String haushaltname, haushaltanschrift, haushaltbeschreibung, haushaltpasswort;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_haushalt_erstellen);
+
+        et_haushalterstellenname = findViewById(R.id.et_haushalterstellenname);
+        et_haushalterstellenanschrift = findViewById(R.id.et_haushalterstellenanschrift);
+        et_haushalterstellenpasswort = findViewById(R.id.et_haushalterstellenpasswort);
+        et_haushalterstellenbeschreibung = findViewById(R.id.editText3);
+        btn_haushalterstellen = findViewById(R.id.btn_haushalterstellen);
+
+        btn_haushalterstellen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                haushaltname = et_haushalterstellenname.getText().toString();
+                haushaltanschrift = et_haushalterstellenanschrift.getText().toString();
+                haushaltbeschreibung = et_haushalterstellenbeschreibung.getText().toString();
+                haushaltpasswort = et_haushalterstellenpasswort.getText().toString();
+            }
+        });
     }
 }
