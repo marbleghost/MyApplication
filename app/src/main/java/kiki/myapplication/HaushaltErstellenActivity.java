51cfd0a942549e8b1403361296337f75d883c064
Kirsten <kapeine@gmx.de>
2018-11-26 16:01:31 +0100

    Bildschirme haben diesen Balken oben nicht mehr

--- app/src/main/java/kiki/myapplication/HaushaltErstellenActivity.java
+++ app/src/main/java/kiki/myapplication/HaushaltErstellenActivity.java
@@ -3,11 +3,14 @@ package kiki.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Objects;
+
 public class HaushaltErstellenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_haushalt_erstellen);
     }
 }
